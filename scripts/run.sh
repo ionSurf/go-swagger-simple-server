dc77e2d0368ae559531209967eca73938108fbe8
Javier Prieto <javier.accounts@fiml.co>
2023-11-02 18:12:28 -0400

    Added remove clause for running container if any.

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,3 +1,4 @@
-#!/usr/bin/bash
+#!/bin/bash
 
-docker run -it -p 8080:8080 --name go-swagger-rest-api go-swagger-simple-server
+docker stop go-swagger-rest-api || true && docker rm go-swagger-rest-api || true
+docker run -it -p 8090:8090 --name go-swagger-rest-api go-swagger-simple-server
